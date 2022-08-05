592e55a923726ae8595e3a6f780c3e01fdbd6a7e
Njivory10 <107516463+Njivory10@users.noreply.github.com>
2022-08-04 21:16:26 -0400

    added screenshots and updated reverseproxy/frontend endpoints

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aacfd097af43548f995027989343a694-1572850734.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
